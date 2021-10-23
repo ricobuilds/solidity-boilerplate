598da6b93223d20547bde1f5d095a8156c5847ef
enrict <enric_trillo@outlook.com>
2021-10-23 18:49:45 +0100

    added basic app sample to components folder

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-gray-800 h-screen">
+      <button className="bg-green-600 text-white font-bold p-3 rounded-md shadow-lg">Blank Project</button>
+    </div>
+  )
+}
+
+export default App
