f7a410bdbc62ef5a176a70e393ca3c4eac8a2522
enrict <enric_trillo@outlook.com>
2021-10-30 22:36:40 +0100

    utils

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import { heroIcon } from './IconUtils'
 
 function App() {
   return (
     <div className="flex justify-center items-center bg-gray-800 h-screen">
-      <button className="bg-green-600 text-white font-bold p-3 rounded-md shadow-lg">Blank Project</button>
+      <button className="bg-green-600 text-white font-bold p-3 rounded-md shadow-lg flex space-x-2"><span>Blank Project</span> {heroIcon}</button>
     </div>
   )
 }
